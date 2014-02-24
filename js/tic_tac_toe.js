10f7ed4165796d793eafa60f65281484dda6d864
Jordan Morano <jordan.morano@gmail.com>
2014-02-24 17:33:50 -0500

    Game functionality complete

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -2,31 +2,40 @@
 // Use the jQuery
 
 $(function(){
-
+  TicTacToe.init();
 });
 
 
 var TicTacToe = {};
 
-TicTacToe.boardReset = (function(){
-  this.board = [
-    [,,,],
-    [,,,],
-    [,,,]
+TicTacToe.board = [
+    [,,],
+    [,,],
+    [,,]
     ];
-})();
+
+TicTacToe.boardReset = function(event) {
+  event.preventDefault();
+  this.board = [
+    [,,],
+    [,,],
+    [,,]
+    ];
+  $('#win-msg').hide();
+  $('.player-1').removeClass('player-1');
+  $('.player-0').removeClass('player-0');
+}
+
+TicTacToe.player = 0;
 
 // alternate player on click and assign starting player
-TicTacToe.changePlayer = (function(){
-  this.player = 0;
-  return function(){
-    if (this.player === 0) {
-      this.player = 1;
-    } else {
-      this.player = 0;
-    }
+TicTacToe.changePlayer = function(){
+  if (this.player === 0) {
+    this.player = 1;
+  } else {
+    this.player = 0;
   }
-})();
+};
 
 // returns true if there is a winning pattern
 TicTacToe.win = function(){
@@ -34,39 +43,49 @@ TicTacToe.win = function(){
     solution7, solution8;
 
   // solutions as booleans
-  solution1 = this.board[0][0] === this.board[0][1] === this.board[0][2];
-  solution2 = this.board[1][0] === this.board[1][1] === this.board[1][2];
-  solution3 = this.board[2][0] === this.board[2][1] === this.board[2][2];
-  solution4 = this.board[0][0] === this.board[1][0] === this.board[2][0];
-  solution5 = this.board[0][1] === this.board[1][1] === this.board[2][1];
-  solution6 = this.board[0][2] === this.board[1][2] === this.board[2][2];
-  solution7 = this.board[0][0] === this.board[1][1] === this.board[2][2];
-  solution8 = this.board[0][2] === this.board[1][1] === this.board[2][0];
+  solution1 = this.board[0][0] === this.board[0][1] && this.board[0][1] === this.board[0][2] && this.board[0][2] !== undefined;
+  solution2 = this.board[1][0] === this.board[1][1] && this.board[1][1] === this.board[1][2] && this.board[1][2] !== undefined;
+  solution3 = this.board[2][0] === this.board[2][1] && this.board[2][1] === this.board[2][2] && this.board[2][2] !== undefined;
+  solution4 = this.board[0][0] === this.board[1][0] && this.board[1][0] === this.board[2][0] && this.board[2][0] !== undefined;
+  solution5 = this.board[0][1] === this.board[1][1] && this.board[1][1] === this.board[2][1] && this.board[2][1] !== undefined;
+  solution6 = this.board[0][2] === this.board[1][2] && this.board[1][2] === this.board[2][2] && this.board[2][2] !== undefined;
+  solution7 = this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2] && this.board[2][2] !== undefined;
+  solution8 = this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0] && this.board[2][0] !== undefined;
 
   // if any true, return true
   return solution1 || solution2 || solution3 || solution4 || solution5 ||
     solution6 || solution7 || solution8;
 };
 
-TicTacToe.move = function(element){
-  var row, col;
-  // Find col and row of piece clicked
-  row = parseInt(element.getAttribute('data-row'));
-  col = parseInt(element.getAttribute('data-col'));
+// Event handlers
+TicTacToe.move = function(event){
+  var row, col, element;
 
-  this.board[row][col] = this.player;
+  element = $(event.target);
 
-  // Check if there is a win
-  if ( TicTacToe.win() ) {
-    // message to congratulate
-    this.msg = 'Player ' + this.player + ' Wins!';
-
-    // div that appears with msg and deactive board clicking
-    return true;
+  if ( element.hasClass('player-1') || element.hasClass('player-0') ) {
+    return false;
   } else {
-    // We change the player
-    TicTacToe.changePlayer();
-    return true;
+    element.addClass('player-' + this.player);
+    // Find col and row of piece clicked
+    row = parseInt(element.attr('data-row'));
+    col = parseInt(element.attr('data-col'));
+
+    this.board[row][col] = this.player;
+
+    // Check if there is a win
+    if ( TicTacToe.win() ) {
+      // message to congratulate
+      $('#win-msg').show();
+      $('#win-msg').html('Player ' + this.player + ' Wins!');
+
+      // div that appears with msg and deactive board clicking
+      return true;
+    } else {
+      // We change the player
+      TicTacToe.changePlayer();
+      return true;
+    }
   }
 };
 
@@ -75,7 +94,8 @@ TicTacToe.move = function(element){
 // Assign event handlers
 TicTacToe.init = function(){
   // Assign event listener to row
-  $('.box_row')
+  $('.box_row').on('click', TicTacToe.move.bind(this));
+  $('#win-msg').on('click', TicTacToe.boardReset.bind(this));
 };
 
 
