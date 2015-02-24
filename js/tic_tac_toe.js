e8a24138f65eff11a4e34426e2fc36b371acc7e3
J.Marx <jennyrutledge@gmail.com>
2015-02-23 23:05:55 -0500

    Add diagonal wins.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -10,22 +10,32 @@ var Game = (function(){
   var player = 0;
 
   var elementColor = function(element){
-  element.click(function(){
-    if(player === 0){
-    $(this).css('background-color', 'red');
-    player = 1;
-  // debugger;
-    // console.log($(this).css('background-color'));
-    }else{
-      $(this).css('background-color', 'black');
-      player = 0;
-    };
-    checkRowWin($(this).data('row'))
-    checkColWin($(this).data('col'));
-    })
+    element.click(function(){
+      if(player === 0){
+        $(this).css('background-color', 'red');
+        player = 1;
+      }else{
+        $(this).css('background-color', 'black');
+        player = 0;
+      };
+      _checkRowWin($(this).data('row'));
+      _checkColWin($(this).data('col'));
+      _checkDiagWin($(this));
+
+    });
   };
 
-  var checkWinner = function(cellColorArr){
+  var _checkDiagWin = function(box_cell){
+    if($(box_cell).data('col')===$(box_cell).data('row')){
+      _checkDiagWinLeft();
+      };
+    //  debugger;
+    if($(box_cell).data('col')+$(box_cell).data('row') === 2){
+    _checkDiagWinRight();
+    };
+  };
+
+  var _checkWinner = function(cellColorArr){
     if((cellColorArr[0] === cellColorArr[1]) && (cellColorArr[1] === cellColorArr[2])){
       alert('Player ' + player + " wins!");
       return(cellColorArr[0] + " wins!");
@@ -46,7 +56,30 @@ var Game = (function(){
   //   });
   // };
 
-  var checkRowWin = function(rowNum){
+  var _checkDiagWinLeft = function(){
+    var box_cells = $('.box_cell').filter(function() {return $(this).data("row") === $(this).data("col")});
+    //debugger;
+    var winCount = 0;
+    var $cellColors = [];
+    for(var j = 0; j < box_cells.size(); j++){
+       $cellColors[j] = $(box_cells[j]).css('background-color');
+     // console.log($(box_cells[j]).data('col'))
+    };
+    _checkWinner($cellColors);
+  };
+
+  var _checkDiagWinRight = function(){
+  var box_cells = $('.box_cell').filter(function() {return $(this).data("row") + $(this).data("col") === 2});
+  var winCount = 0;
+    var $cellColors = [];
+    for(var j = 0; j < box_cells.size(); j++){
+       $cellColors[j] = $(box_cells[j]).css('background-color');
+     // console.log($(box_cells[j]).data('col'))
+    };
+    _checkWinner($cellColors);
+  };
+
+  var _checkRowWin = function(rowNum){
     var box_cells = $('.box_cell').filter(function() {return $(this).data("row") === rowNum });
     var winCount = 0;
     var $cellColors = [];
@@ -54,21 +87,20 @@ var Game = (function(){
        $cellColors[j] = $(box_cells[j]).css('background-color');
      // console.log($(box_cells[j]).data('col'))
     };
-    checkWinner($cellColors);
+    _checkWinner($cellColors);
   };
 
-  var checkColWin = function(colNum){
+  var _checkColWin = function(colNum){
     var box_cells = $('.box_cell').filter(function() {return $(this).data("col") === colNum });
     var winCount = 0;
     var $cellColors = [];
     for(var j = 0; j < box_cells.size(); j++){
        $cellColors[j] = $(box_cells[j]).css('background-color');
     };
-    checkWinner($cellColors);
+    _checkWinner($cellColors);
   };
   return{
-    elementColor: elementColor,
-    checkRowWin: checkRowWin
+    elementColor: elementColor
   };
 
 })();
