d51ccff2830aeb41e1d6f212853cec4ae3642e23
Max Blaushild <blaushmild@gmail.com>
2015-02-23 22:31:45 -0500

    refactored, significantly more DRY. looking real purty

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -17,30 +17,20 @@ $(document).ready(function(){
 
   var playerOneTurn = function(ele){
     ele.css('background-color','red').attr('data-player', 'player1');
-    playerOneCheck();
+    playerCheck("player1");
   };
 
   var playerTwoTurn = function(ele){
     ele.css('background-color','blue').attr('data-player', 'player2');
-    playerTwoCheck();
+    playerCheck("player2");
   };
 
-  var playerOneCheck = function(){
-    var playerOne = $('td[data-player="player1"]');
+  var playerCheck = function(player){
+    var playerOne = $('td[data-player="' + player + '"]');
     var row = jQuery.map(playerOne, function(cell) {return Number(cell.dataset.row) });
     var col = jQuery.map(playerOne, function(cell) {return Number(cell.dataset.col) });
-    if (counter(col).indexOf(3) > -1) { alert('Red player has won!')};
-    if (counter(row).indexOf(3) > -1) { alert('Red player has won!')};
-    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)) { alert('Red player has won!')};
-  };
-
-  var playerTwoCheck = function(){
-    var playerTwo = $('td[data-player="player2"]');
-    var row = jQuery.map(playerTwo, function(cell) {return Number(cell.dataset.row) });
-    var col = jQuery.map(playerTwo, function(cell) {return Number(cell.dataset.col) });
-    if (counter(col).indexOf(3) > -1) { alert('Blue player has won!')};
-    if (counter(row).indexOf(3) > -1) { alert('Blue player has won!')};
-    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)){ alert('Blue player has won!')};
+    if ((counter(col).indexOf(3) > -1) || (counter(row).indexOf(3) > -1)) { alert(player + ' has won!')};
+    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)) { alert(player + ' has won!')};
   };
 
   $('td').on('click', function() {
