a48171c173711bbc619c238f1cfce67bbdd80333
Max Blaushild <blaushmild@gmail.com>
2015-02-23 22:36:57 -0500

    resolves namespace issue. working gewd

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -26,9 +26,9 @@ $(document).ready(function(){
   };
 
   var playerCheck = function(player){
-    var playerOne = $('td[data-player="' + player + '"]');
-    var row = jQuery.map(playerOne, function(cell) {return Number(cell.dataset.row) });
-    var col = jQuery.map(playerOne, function(cell) {return Number(cell.dataset.col) });
+    var playerMoves = $('td[data-player="' + player + '"]');
+    var row = jQuery.map(playerMoves, function(cell) {return Number(cell.dataset.row) });
+    var col = jQuery.map(playerMoves, function(cell) {return Number(cell.dataset.col) });
     if ((counter(col).indexOf(3) > -1) || (counter(row).indexOf(3) > -1)) { alert(player + ' has won!')};
     if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)) { alert(player + ' has won!')};
   };
