0c80972aa768683c2cb79df16fff00c8db10cc25
JC Gruta <jase.aturg@gmail.com>
2015-02-23 22:54:25 -0500

    Finished about half of the assignment.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,2 +1,26 @@
-// Your code here
-// Use the jQuery
+$(document).ready(function() {
+
+  var lastColor = 'red';
+
+  // gives me all elements with a class of box_cell
+  $('table tbody tr').on('click', 'td', function(event){
+
+    var done = $(this).data('done');
+    if(!done && lastColor == 'red'){
+      // set to black
+      $(this).css('background-color', 'black');
+      lastColor = 'black'
+    }else if(!done) {
+      // set to red
+     $(this).css('background-color', 'red');
+     lastColor = 'red';
+    }
+
+    $(this).data('done', true);
+
+
+  });
+
+});
+
+
