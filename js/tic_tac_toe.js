9c214475bf8b9e20746412a2eb8acf8a06c75d91
Max Blaushild <blaushmild@gmail.com>
2015-02-24 10:04:52 -0500

    fixes diagonal winchecker

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -16,8 +16,13 @@ $(document).ready(function(){
   };
 
   var playerTurn = function(ele, color, player){
-    ele.css('background-color',color).attr('data-player', player);
+    ele.css('background-color',color).attr('data-player', player).data('player', player);
     playerCheck(player);
+  };
+
+  var diagonalChecker = function(player) {
+    if (($('#box_0_0').data().player === player) && ($('#box_1_1').data().player === player) && ($('#box_2_2').data().player === player)) { alert(player + ' has won!')};
+    if (($('#box_0_2').data().player === player) && ($('#box_1_1').data().player === player) && ($('#box_2_0').data().player === player)) { alert(player + ' has won!')};
   };
 
   var playerCheck = function(player){
@@ -25,10 +30,10 @@ $(document).ready(function(){
     var row = jQuery.map(playerMoves, function(cell) {return Number(cell.dataset.row) });
     var col = jQuery.map(playerMoves, function(cell) {return Number(cell.dataset.col) });
     if ((counter(col).indexOf(3) > -1) || (counter(row).indexOf(3) > -1)) { alert(player + ' has won!')};
-    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)) { alert(player + ' has won!')};
+    diagonalChecker(player);
   };
 
-  $('td').on('click', function() {
+  $('td').one('click', function() {
     (turn % 2 === 0) ? playerTurn($(this), "red", "player1") : playerTurn($(this), "blue", "player2");
     turn = turn + 1;
   });
