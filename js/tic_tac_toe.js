d51b5530edfd818e3b0497436be64360701392d9
Max Blaushild <blaushmild@gmail.com>
2015-02-23 21:59:55 -0500

    trimmed some redundent logic to get this sucka under 50 lives

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -3,17 +3,16 @@ $(document).ready(function(){
   var turn = 0;
 
   var counter = function(arr) {
-    var b = [], prev;
-    arr.sort();
+    var count = [], prev;
     for ( var i = 0; i < arr.length; i++ ) {
         if ( arr[i] !== prev ) {
-            b.push(1);
+            count.push(1);
         } else {
-            b[b.length-1]++;
+            count[count.length-1]++;
         }
         prev = arr[i];
     }
-    return b;
+    return count;
   };
 
   var playerOneTurn = function(ele){
@@ -30,18 +29,18 @@ $(document).ready(function(){
     var playerOne = $('td[data-player="player1"]');
     var row = jQuery.map(playerOne, function(cell) {return Number(cell.dataset.row) });
     var col = jQuery.map(playerOne, function(cell) {return Number(cell.dataset.col) });
-    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && counter(col).indexOf(3) > -1) { alert('Red player has won!')};
-    if ((col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1) && counter(row).indexOf(3) > -1) { alert('Red player has won!')};
-    if ((col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1) && (row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1)) { alert('Red player has won!')};
+    if (counter(col).indexOf(3) > -1) { alert('Red player has won!')};
+    if (counter(row).indexOf(3) > -1) { alert('Red player has won!')};
+    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)) { alert('Red player has won!')};
   };
 
   var playerTwoCheck = function(){
     var playerTwo = $('td[data-player="player2"]');
     var row = jQuery.map(playerTwo, function(cell) {return Number(cell.dataset.row) });
     var col = jQuery.map(playerTwo, function(cell) {return Number(cell.dataset.col) });
-    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && counter(col).indexOf(3) > -1) { alert('Blue player has won!')};
-    if ((col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1) && counter(row).indexOf(3) > -1){ alert('Blue player has won!')};
-    if ((col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1) && (row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1)) { alert('Blue player has won!')};
+    if (counter(col).indexOf(3) > -1) { alert('Blue player has won!')};
+    if (counter(row).indexOf(3) > -1) { alert('Blue player has won!')};
+    if ((row.indexOf(0) > -1 && row.indexOf(1) > -1 && row.indexOf(2) > -1) && (col.indexOf(0) > -1 && col.indexOf(1) > -1 && col.indexOf(2) > -1)){ alert('Blue player has won!')};
   };
 
   $('td').on('click', function() {
